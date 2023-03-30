74a8571291bdf43275cc1dfb0bf2b7d9d1a689ec
chxrvey <109527161+chxrvey@users.noreply.github.com>
2023-03-30 11:36:30 +0100

    components/navbar fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <nav>
-      <Link href="/">
+      <Link href="/" passHref>
         <div className="logo">
           <Image src="/LCL Logo.png" alt="site logo" width={250} height={100} />
           <span className="logo-text">London Comedy Lunch</span>
